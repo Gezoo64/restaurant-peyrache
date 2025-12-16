fc6f18b96c2d4898f97db96342d2aa5ba940a571
Gezoo64 <peyrachearnaud@gmail.com>
2025-12-16 15:54:43 +0100

    update appliquerReduction js

--- addiction.test.js
+++ addiction.test.js
@@ -2,7 +2,7 @@ const {calculerAddition, appliquerReduction} = require("./addition.js");
 
 test("Test calculerAddition(15, 5) => 20", ()=>{
     expect(calculerAddition(15, 5)).toBe(20)
-})
+});
 test("Test appliquerReduction(100, 10) => 90", ()=>{
-    expect(appliquerReduction(100, 100)).toBe(90)
-})
+    expect(appliquerReduction(100, 10)).toBe(90)
+});
